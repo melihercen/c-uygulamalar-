9f24d7fc75ee34392f1dbe0c3d190ba8e1a8d854
melihercen <127030992+melihercen@users.noreply.github.com>
2024-05-15 20:50:22 +0300

    calculator.c

--- calculator.c
+++ calculator.c
@@ -1,108 +1,173 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+void exponentiate(int base,int upper)
+{
+    if(upper==0)
+    {
+        printf("%d to the power of %d = 1",base,upper);
+    }
+    else{
+    int result=base;
+    //taban belirtilen us ifadesi kadar dongude carpma
+    //multiply in the loop until the base specified exponent expression
+    for(int i=1;i<upper;i++)
+    {
+        
+        result*=base;
+        
+    }
+    printf("%d to the power of %d =",base,upper);
+    printf("%d",result);
+    }
+}
 int main()
 {
     float num1,num2,num3,num4;
     float operation_num;
     float result;
     char operation_type;
-    printf("How many numbers do you want to operation: ");
-    scanf("%f",&operation_num);
-    
-    //Kaç sayılı bir işlem yapılcağı öğrenilir
-    //How many numbered operations will be carried out
-    if (operation_num>=2 || operation_num==4)
+    int operation;
+    int choice;
+    int base,upper;
+    int squareroot;
+    float sqrtresult;
+    printf("1-Basic processing(+,-,/,*)\n2-Exponentiate /Extraction \n");
+    scanf("%d",&operation);
+    if(operation==2)
     {
-        printf("Input your operation numbers: ");
-        if (operation_num==2)
+        
+        printf("1-Exponentiate \n2-Extraction\n");
+        scanf("%d",&choice);
+        switch (choice)
         {
-            scanf("%f %f",&num1,&num2);
+        case 1:
+            printf("Enter base force: \n");
+            scanf("%d",&base);
+            printf("Enter the upper force:\n");
+            scanf("%d",&upper);
+            
+            exponentiate(base,upper);
+            break;
+        case 2:
+            printf("Enter the number to be square-rooted:  ");
+            scanf("%d",&squareroot);
+            sqrtresult=sqrt(squareroot);
+            printf("square root of %d = %f",squareroot,sqrtresult);
+            break;       
+        default:
+            break;
         }
-        else if(operation_num==3)
+    }
+
+    else if(operation==1)
+    {
+        printf("How many numbers do you want to operation: ");
+        scanf("%f",&operation_num);
+        
+        //Kaç sayılı bir işlem yapılcağı öğrenilir
+        //How many numbered operations will be carried out
+        if (operation_num>=2 || operation_num==4)
         {
-            scanf("%f %f %f",&num1,&num2,&num3);
-        }
-        else if(operation_num==4)
-        {
-            scanf("%f %f %f %f",&num1,&num2,&num3,&num4);
+            printf("Input your operation numbers: ");
+            if (operation_num==2)
+            {
+                scanf("%f %f",&num1,&num2);
+            }
+            else if(operation_num==3)
+            {
+                scanf("%f %f %f",&num1,&num2,&num3);
+            }
+            else if(operation_num==4)
+            {
+                scanf("%f %f %f %f",&num1,&num2,&num3,&num4);
+            }
+            else
+            {
+                printf("Wrong input");
+            }
+        //Hangi tip bir işlem yapılcağı öğrenilir
+        //Learned which type operation will made
+            printf("Which type operation (+,-,*,/): ");
+            scanf(" %c",&operation_type);
+        //Switch ile işlemler oluşturulur
+        //With switch make the operation
+            switch (operation_type)
+            {
+                case '+':
+                    if(operation_num==2)
+                    {
+                    result=num1+num2;
+                    printf("Operation result: %f\n",result);
+                    }
+                    else if(operation_num==3)
+                    {
+                    result=num1+num2+num3;
+                    printf("Operation result: %f\n",result);
+                    }
+                    else if(operation_num==4)
+                    {
+                    result=num1+num2+num3+num4;
+                    printf("Operation result: %f\n",result);
+                    }
+                    break;
+                case '-':
+                    if(operation_num==2)
+                    {
+                    result=num1-num2;
+                    printf("Operation result: %f\n",result);
+                    }
+                    else if(operation_num==3)
+                    {               
+                    result=num1-num2-num3;
+                    printf("Operation result: %f\n",result);
+                    }
+                    else if(operation_num==4){
+                    result=num1-num2-num3-num4;
+                    printf("Operation result: %f\n",result);
+                    }
+                        break;
+                case '*':
+                    if(operation_num==2)
+                    {
+                    result=num1*num2;
+                    printf("Operation result: %f\n",result);
+                    }
+                    else if(operation_num==3)
+                    {
+                    result=num1*num2*num3;
+                    printf("Operation result: %f\n",result);
+                    }
+                    else if(operation_num==4)
+                    {
+                    result=num1*num2*num3*num4;
+                    printf("Operation result: %f\n",result);
+                    }
+                        break;
+                case '/':
+                    if(operation_num==2)
+                    {
+                    result=(float)num1/num2;
+                    printf("Operation result: %.2f\n",result);
+                    }
+                        break;
+            
+                default:
+                    printf("Wrong input\n");
+                    break;
+                }
         }
         else
         {
             printf("Wrong input");
         }
-    //Hangi tip bir işlem yapılcağı öğrenilir
-    //Learned which type operation will made
-        printf("Which type operation (+,-,*,/): ");
-        scanf(" %c",&operation_type);
-    //Switch ile işlemler oluşturulur
-    //With switch make the operation
-        switch (operation_type)
-        {
-            case '+':
-                if(operation_num==2)
-                {
-                result=num1+num2;
-                printf("Operation result: %f\n",result);
-                }
-                else if(operation_num==3)
-                {
-                result=num1+num2+num3;
-                printf("Operation result: %f\n",result);
-                }
-                else if(operation_num==4)
-                {
-                result=num1+num2+num3+num4;
-                printf("Operation result: %f\n",result);
-                }
-                break;
-            case '-':
-                if(operation_num==2)
-                {
-                result=num1-num2;
-                printf("Operation result: %f\n",result);
-                }
-                else if(operation_num==3)
-                {               
-                result=num1-num2-num3;
-                printf("Operation result: %f\n",result);
-                }
-                else if(operation_num==4){
-                result=num1-num2-num3-num4;
-                printf("Operation result: %f\n",result);
-                }
-                    break;
-            case '*':
-                if(operation_num==2)
-                {
-                result=num1*num2;
-                printf("Operation result: %f\n",result);
-                }
-                else if(operation_num==3)
-                {
-                result=num1*num2*num3;
-                printf("Operation result: %f\n",result);
-                }
-                else if(operation_num==4)
-                {
-                result=num1*num2*num3*num4;
-                printf("Operation result: %f\n",result);
-                }
-                    break;
-            case '/':
-                if(operation_num==2)
-                {
-                result=(float)num1/num2;
-                printf("Operation result: %.2f\n",result);
-                }
-                    break;
-        
-            default:
-                printf("Wrong input\n");
-                break;
-            }
     }
     else
-    {
-        printf("Wrong input");
-    }
+        {
+            printf("Wrong input");
+        }
+
     return 0;
 }
